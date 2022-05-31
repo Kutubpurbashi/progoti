56a40323cb4f1696e738c3e5db7ab8ea1ab9b099
Moin <ourmoin@gmail.com>
2022-05-31 12:13:05 +0600

    Update about.js

--- data/about.js
+++ data/about.js
@@ -1,6 +1,6 @@
 const categoryData = [
     {
-        photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/277352904_509542380781482_9103892823481662625_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8631f5&_nc_eui2=AeFY_X4oGCqdkdy4_0EzRWkmTNOuYSgFbZJM065hKAVtklc5URaCdAJ6UI5EbBq8jNJN2FmGtspDNJtaXpKDhidI&_nc_ohc=sZ60SILEnlAAX-Zrc7k&_nc_ht=scontent.fdac41-1.fna&oh=00_AT_azpBZtpZE9AQPE-VeJ1maryiNQTJIOwBE4QV0D1tqXA&oe=628D45C2",
+        photo: "https://drive.google.com/file/d/1BXeMtY4zrlQQmsTXz4LNxtv-ymkSbvpS/view?usp=sharing",
         about: "স্বনির্ভর বাংলাদেশ গড়ার লক্ষ্যে সৎ, মেধাবী ও দেশপ্রেমিক নাগরিক তৈরির অঙ্গীকার"
     }
 ];
