b3c5c5fadf4f9b72d031455a73d7b991506039dc
Moin <ourmoin@gmail.com>
2022-05-31 11:51:50 +0600

    Update work.js

--- data/work.js
+++ data/work.js
@@ -1,7 +1,7 @@
 const categoryData = [
     {
         albumLink: "https://www.facebook.com/media/set/?set=a.4721231151307185&type=3",
-        photo: "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/268993091_4721330404630593_8373798683146353782_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=0debeb&_nc_eui2=AeF_xdf7vSGQR8CsVisIZKFoR34gnGNB3BdHfiCcY0HcFzLuEaFwm1OKNmGYt62kjMFXcZrJFB0K3OabljHlZHy3&_nc_ohc=NwO9mjw-4LwAX9jeHYO&tn=SGASXOK3c_jEIybK&_nc_ht=scontent.fdac41-1.fna&oh=00_AT93P_nJ6KmPwUznwoqZBVX9yq5CcSug8XiqPUROcq7MIg&oe=628DB72E",
+        photo: "https://scontent-ccu1-1.xx.fbcdn.net/v/t39.30808-6/269290452_4721328854630748_7904584057057327454_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=0debeb&_nc_eui2=AeHOGNI_wvERv38zIUNte0B8nA7nQh-7pb2cDudCH7ulvamjt79eCnCbAgObwCcz3hehdUGA_T0OVLupyPOL3HvJ&_nc_ohc=YkBdCEhIQ8AAX_4ntEP&_nc_ht=scontent-ccu1-1.xx&oh=00_AT8xGxJldV981AxJmIgMoTDYPEThKVL-2D_jH28ntw5BBQ&oe=629B889F",
         title: "আন্তর্জাতিক মাতৃভাষা উদযাপন",
         description: "২১শে ফেব্রুয়ারি বাঙালি জাতির চির প্রেরণা ও অবিস্মরণীয় একটি দিন। ১৯৫২ সালের একুশে ফেব্রুয়ারি ছিল ঔপনিবেশিক প্রভুত্ব ও শাসন-শোষণের বিরুদ্ধে বাঙালির প্রথম প্রতিরোধ এবং জাতীয় চেতনার প্রথম উন্মেষ। কুতুবপুর 'সম্মিলিত সামাজিক সংগঠন পরিষদ' এর উদ্যোগে ভাষা শহীদদের প্রতি শ্রদ্ধা নিবেদন করা হয়",
         time: "21 Feb 2022"
