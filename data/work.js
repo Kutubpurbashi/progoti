c0e1d461e322a97bcf4bf8e12e72ff58ac7c9648
MohammadMonirHossain <105538722+MohammadMonirHossain@users.noreply.github.com>
2022-05-31 15:10:48 +0600

    ছবি পরিবর্তন

--- data/work.js
+++ data/work.js
@@ -1,7 +1,7 @@
 const categoryData = [
     {
-        albumLink: "https://www.facebook.com/media/set/?set=a.4721231151307185&type=3",
-        photo: "https://scontent-ccu1-1.xx.fbcdn.net/v/t39.30808-6/269290452_4721328854630748_7904584057057327454_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=0debeb&_nc_eui2=AeHOGNI_wvERv38zIUNte0B8nA7nQh-7pb2cDudCH7ulvamjt79eCnCbAgObwCcz3hehdUGA_T0OVLupyPOL3HvJ&_nc_ohc=YkBdCEhIQ8AAX_4ntEP&_nc_ht=scontent-ccu1-1.xx&oh=00_AT8xGxJldV981AxJmIgMoTDYPEThKVL-2D_jH28ntw5BBQ&oe=629B889F",
+        albumLink: "https://m.facebook.com/story.php?story_fbid=pfbid0zG9H2oKHnFFZNTRk5dieiB842CtGY16itsPmUwkD849qNqT6ibzoB7N7Nn59si7ul&id=100002610409739",
+        photo: "https://m.facebook.com/photos/viewer/?photoset_token=pcb.4951781058252192&photo=4951775454919419&profileid=100002610409739&source=49&_ft_=qid.-6242637654016568098%3Amf_story_key.4951781058252192%3Atop_level_post_id.4951781058252192%3Atl_objid.4951781058252192%3Acontent_owner_id_new.100002610409739%3Athrowback_story_fbid.4951781058252192%3Aphoto_attachments_list.%5B4951775188252779%2C4951775454919419%2C4951775624919402%2C4951775828252715%2C4951776254919339%5D%3Astory_location.4%3Astory_attachment_style.album%3Aent_attachement_type.PhotoSetAttachment%3Aapp_id.350685531728%3Aprofile_id.100002610409739%3Aprofile_relationship_type.1%3Aactrs.100002610409739%3Athid.100002610409739%3A306061129499414%3A2%3A1641024000%3A1672559999%3A5515963654524156308%3A%3A%3Aftmd_400706.111111l&__tn__=EH-R ",
         title: "আন্তর্জাতিক মাতৃভাষা উদযাপন",
         description: "২১শে ফেব্রুয়ারি বাঙালি জাতির চির প্রেরণা ও অবিস্মরণীয় একটি দিন। ১৯৫২ সালের একুশে ফেব্রুয়ারি ছিল ঔপনিবেশিক প্রভুত্ব ও শাসন-শোষণের বিরুদ্ধে বাঙালির প্রথম প্রতিরোধ এবং জাতীয় চেতনার প্রথম উন্মেষ। কুতুবপুর 'সম্মিলিত সামাজিক সংগঠন পরিষদ' এর উদ্যোগে ভাষা শহীদদের প্রতি শ্রদ্ধা নিবেদন করা হয়",
         time: "21 Feb 2022"
